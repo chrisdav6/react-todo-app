28cbf8e82034206ed59339e694c845028f2e7987
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-21 14:51:22 -0400

    Got todo state working

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { firebaseConfig } from '../firebase';
 
 const App = () => {
-  return <div className='App'>App</div>;
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleOnChange = e => {
+    const inputValue = e.target.value;
+    setInput(inputValue);
+  };
+
+  const handleSubmit = () => {
+    setTodos(prev => {
+      return [...prev, input];
+    });
+    setInput('');
+  };
+
+  return (
+    <div className='App'>
+      <h1>Todo App</h1>
+      <input type='text' value={input} onChange={handleOnChange} />
+      <button onClick={handleSubmit}>Submit</button>
+
+      {todos.map(todo => (
+        <p>{todo}</p>
+      ))}
+    </div>
+  );
 };
 
 export default App;
